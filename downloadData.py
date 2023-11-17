fd92e2ce8f39bfc9ee7e0ad7ee998e6fa882a9ab
lcamgalindo <lgalindo@kwl.ca>
2023-11-16 17:07:33 -0800

    fix issue with url.

--- downloadData.py
+++ downloadData.py
@@ -10,7 +10,7 @@ import os
 # Variables to change
 stationID = 1207
 dayData = 14 # Only change if you are actually downloading for one day, otherwise keep it since the url requires it.
-stationName = 'sparwood5'
+stationName = 'sparwood6'
 startYear = 1990
 endYear = 1991
 timeframe = 1 # 1: Hourly, 2: Daily, 3: Montly
@@ -27,11 +27,13 @@ yearData = [int(x) for x in np.linspace(startYear,endYear,nYears).tolist()]
 for year in yearData:
 	for month in monthData:
 		print(f'{year}-{month}')
-		url = 'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&time=utc&stationID='+str(stationID)+'&Year='+str(year)+'&Month='+str(month)+'&Day='+str(dayData)+'&timeframe='+str(timeframe)+'&submit=Download+Data'
+		url = 'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID='+str(stationID)+'&Year='+str(year)+'&Month='+str(month)+'&Day='+str(dayData)+'&timeframe='+str(timeframe)+'&submit=Download+Data'
 		# Add option to do UTC or LST
 		source = urllib.request.urlopen(url).read()
+		# print(url)
+		# sys.exit()
 
-		filename = 'data/'+stationName+'/stationID_'+str(stationID)+'year_'+str(year)+'allMonths.csv'
+		filename = 'data/'+stationName+'/stationID_'+str(stationID)+'year_'+str(year)+'_month_'+str(month)+'.csv'
 		with open(filename,'wb') as f:
 			f.write(source)
 
